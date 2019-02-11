f6f31018f95e7945ecd5e878c421b0b7ad2b5426
kwangho-park (solt) <44250982+kwangho-park@users.noreply.github.com>
2019-02-11 22:02:23 +0900

    Add files via upload

---
+++ src/chap11/Member.java
@@ -0,0 +1,15 @@
+// java bean
+
+package chap11;
+
+public class Member {
+
+	private String id = "pkship0410";
+	private int pw = 1234;
+	
+	public String getId() {	return this.id; }
+	public void setId(String id) { this.id = id; }
+	
+	public int getPw() {	return this.pw; }
+	public void setPw(int pw) { this.pw = pw; }	
+}
